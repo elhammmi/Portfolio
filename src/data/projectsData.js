efb620d873ed6a8049a6bd41fd82152bef31d702
elhammmi <elham.mirzavand@gmail.com>
2024-05-14 23:28:41 +0200

    Add new project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,4 +1,5 @@
 import Spotify from '../assets/projects/spotify-clone.jpg'
+import SimpleBlog from '../assets/projects/simple-blog.jpg'
 
 
 export const data=[
@@ -9,5 +10,12 @@ export const data=[
         github:"https://github.com/elhammmi/Spotify-Clone",
         live:"http://spotify-clone.elhammirzavand.se/",
     },
+    {
+        id:2,
+        name: "Simple Blog Applicaion",
+        image: SimpleBlog,
+        github:"https://github.com/elhammmi/blog-app",
+        live:"https://elhammmi.github.io/blog-app/"
+    }
   
 ]
